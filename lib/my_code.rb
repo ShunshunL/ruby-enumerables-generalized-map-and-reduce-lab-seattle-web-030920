5cd6ae19fced44420fa9fd398296e4bd9274b646
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-13 00:33:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # Your Code Here
+require 'pp'
+def map(source_array)
+  index = 0 
+  new_array = []
+  while index < source_array.length 
+    new_array << yield(source_array[index])
+    index += 1 
+  end
+  new_array
+end
