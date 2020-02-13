95a061d436244ca43679dc37a1034b550d6b23d0
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-13 01:19:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,11 +11,6 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point = nil)
-  index = 0 
-  return_value = 0 
-  while index < source_array.length
-    return_value += yield(source_array[index] + source_array[index + 1])
-    index += 2
-  end 
-  return starting_point + return_value
+  if starting_point
+    
 end 
